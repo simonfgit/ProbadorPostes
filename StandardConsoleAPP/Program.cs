41cf7de02cdaaa66ed285c8d13af764cdb40bb0a
Luis Valle <vluis@eternet.cc>
2018-07-11 15:11:28 -0300

    Version 2.0
    Imprimo todo en pantalla con Logging

--- StandardConsoleAPP/Program.cs
+++ StandardConsoleAPP/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Eternet.Mikrotik;
+﻿using Eternet.Mikrotik;
 using Eternet.Mikrotik.Entities;
 using Eternet.Mikrotik.Entities.Interface;
 using Eternet.Mikrotik.Entities.Ip;
 using Microsoft.Extensions.Configuration;
 using Serilog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using Log = Serilog.Log;
 
 namespace ProbadorPostes
@@ -52,18 +53,36 @@ namespace ProbadorPostes
 
             var runningethers = etherReader.GetAll().Where(p => p.Running == true);
 
+            var interfacesToTest = new List<(string, string)>();
+
             foreach (var ether in runningethers)
             {
                 if (neighReader.GetAll().Count(i => i.Interface.Contains(ether.Name)) == 1)
                 {
                     var neigh = neighReader.Get(n => n.Interface.Contains(ether.Name));
-                    Console.WriteLine($"En la interface {neigh.Interface} se encuentra un equipo {neigh.Board} con la MAC {neigh.MacAddress}");
+                    Log.Logger.Information("En la interface {Interface} se encuentra un equipo {Modelo} con la MAC {MacAddress}", neigh.Interface, neigh.Board, neigh.MacAddress);
+                    if (neigh.Address4 != "")
+                    {
+                        interfacesToTest.Add((neigh.Interface, neigh.Address4));
+                        Log.Logger.Information("Se agrego a la lista de interfaces a probar a Iface {Interface} con IP {Address}", neigh.Interface, neigh.Address4);
+                    }
+                    else
+                    {
+                        Log.Logger.Information("La Iface {Interface} no tiene IP y NO se agrega a la lista de pruebas", neigh.Interface);
+                        Log.Logger.Information("El vecino en la {Interface} no tiene IP", neigh.Interface);
+                    }
                 }
                 else
                 {
-                    Console.WriteLine($"La interface {ether.Name} esta running y no ve ningun vecino");
+                    Log.Logger.Information("La interface {Interface} esta running y contiene varios o NO contiene ningun vecino/vecinos", ether.Name);
                 }
             }
+
+            foreach (var ethtotest in interfacesToTest)
+            {
+                Console.WriteLine(ethtotest.ToString());
+            }
+
             connection.Dispose();
             Log.Logger.Information("Done!, press any key to end");
             Console.ReadLine();
