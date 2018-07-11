0ede99a19ddee13c4b2c8248a19f421bc7bcf802
Luis Valle <vluis@eternet.cc>
2018-07-11 11:27:34 -0300

    Descarga Aplicacion BASE

---
+++ StandardConsoleAPP/ConfigurationClass.cs
@@ -0,0 +1,14 @@
+﻿namespace StandardConsoleApp
+{
+    public class ConfigurationClass
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+        public string AreaName { get; set; }
+        public string Host { get; set; }
+        public string ApiUser { get; set; }
+        public string ApiPass { get; set; }
+        public string[] ExcludedInterfaces { get; set; }
+
+    }
+}
---
+++ StandardConsoleAPP/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Log = Serilog.Log;
+
+namespace StandardConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args == null) throw new ArgumentNullException(nameof(args));
+
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
+                .AddJsonFile("appsettings.json", optional: false);
+
+            var cfg = builder.Build();
+
+            var mycfg = new ConfigurationClass();
+            cfg.GetSection("ConfigurationClass").Bind(mycfg);
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(cfg)
+                .CreateLogger();
+        }
+    }
+}
