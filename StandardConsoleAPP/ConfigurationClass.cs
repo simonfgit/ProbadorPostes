59ea54a5716fba4b7c433068ecc6dfd6d97c1153
Luis Valle <vluis@eternet.cc>
2018-07-11 11:52:31 -0300

    Obtengo vecinos y muestro en pantalla

--- StandardConsoleAPP/ConfigurationClass.cs
+++ StandardConsoleAPP/ConfigurationClass.cs
@@ -2,13 +2,9 @@
 {
     public class ConfigurationClass
     {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string AreaName { get; set; }
         public string Host { get; set; }
         public string ApiUser { get; set; }
         public string ApiPass { get; set; }
-        public string[] ExcludedInterfaces { get; set; }
-
+        
     }
 }
--- StandardConsoleAPP/Program.cs
+++ StandardConsoleAPP/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.IO;
+using Eternet.Mikrotik;
+using Eternet.Mikrotik.Entities;
+using Eternet.Mikrotik.Entities.Ip;
 using Microsoft.Extensions.Configuration;
 using Serilog;
 using Log = Serilog.Log;
@@ -8,7 +11,14 @@ namespace ProbadorPostes
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private static ITikConnection GetMikrotikConnection(string host, string user, string pass)
+        {
+            var connection = ConnectionFactory.CreateConnection(TikConnectionType.Api);
+            connection.Open(host, user, pass);
+            return connection;
+        }
+
+        private static void Main(string[] args)
         {
             if (args == null) throw new ArgumentNullException(nameof(args));
 
@@ -25,6 +35,19 @@ namespace ProbadorPostes
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(cfg)
                 .CreateLogger();
+
+            var connection = GetMikrotikConnection(mycfg.Host, mycfg.ApiUser, mycfg.ApiPass);
+
+            var neighReader = connection.CreateEntityReader<IpNeighbor>();
+
+            var neighbors = neighReader.GetAll();
+
+            foreach (var neigh in neighbors)
+            {
+                Console.WriteLine($"En la interface {neigh.Interface} se encuentra un equipo {neigh.Board} con la MAC {neigh.MacAddress}");
+            }
+            Console.WriteLine("Press any key to end");
+            Console.ReadLine();
         }
     }
 }
